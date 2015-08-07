7823fc9e27376426ca209ee2685cda052a4b6a0e
pmi <pmi@bls.com>
2015-08-07 09:52:35 +0200

    Small fix for refreshing job color

--- public/js/ko.bindings.js
+++ public/js/ko.bindings.js
@@ -1,5 +1,5 @@
 ko.bindingHandlers.jobStatusColor = {
-    init: function(element, valueAccessor, allBindings, viewModel, bindingContext) {
+    update: function(element, valueAccessor, allBindings, viewModel, bindingContext) {
         var cfg = {
                     disabled: 'black',
                     blue: 'green',
@@ -8,10 +8,5 @@ ko.bindingHandlers.jobStatusColor = {
                 };
 //        $(element).css('background-color', ko.unwrap(valueAccessor()));
         $(element).addClass(cfg[ko.unwrap(valueAccessor())]);
-    },
-    update: function(element, valueAccessor, allBindings, viewModel, bindingContext) {
-        // This will be called once when the binding is first applied to an element,
-        // and again whenever any observables/computeds that are accessed change
-        // Update the DOM element based on the supplied values here.
     }
 }
