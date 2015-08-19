80cab42135575d88acedd49745bba46934a8a305
pmi <pmi@bls.com>
2015-08-19 12:30:37 +0200

    Added some simple and/not filter to the name param

--- routes/radiator.js
+++ routes/radiator.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var format = require('string-format');
 var jenkinsApi = require('jenkins-api');
+var filter = require('../utils/filter');
 var config = require('./config');
 var router = express.Router();
 
@@ -9,7 +10,7 @@ var storedFilterParams = {};  //holding params per referer/ip/request
 var jenkins;
 
 config.loadConfig(function(err, cfg){
-console.dir(config);
+    console.dir(config);
     jenkinsConnect();
 });
 
@@ -19,19 +20,19 @@ router.get('/', function(req, res, next){
     res.render('radiator');
 });
 
-router.get('^(/name/:name?)?(/type/:type?)?', function(req, res){
+router.get('^(/name/:name?)?(/type/:type?)?(/detailed/:detailed?)?', function(req, res){
     console.dir(req.params);
     //add checking against empty/bad config and redirect to setup then?
-    storedFilterParams[req.get('host')] = {name: req.params.name, type: req.params.type};
+    storedFilterParams[req.get('host')] = {name: req.params.name, type: req.params.type, detailed: req.params.detailed};
     console.dir(storedFilterParams);
     res.location('/radiator');
     res.render('radiator');
 });
 
-router.get('^\/jobs(\/name\/:name?\/type\/:type?)?', function(req, res, next) {
+router.get('^\/jobs(\/name\/:name?\/type\/:type?\/detailed\/:detailed?)?', function(req, res, next) {
     console.dir(req.params);
     if(req.params.length > 0) {
-        getJobs(req, res, {name: req.params.name, type: req.params.type});
+        getJobs(req, res, {name: req.params.name, type: req.params.type, detailed: req.params.detailed});
     } else if(storedFilterParams[req.get('host')] && (storedFilterParams[req.get('host')].name || storedFilterParams[req.get('host')].type)) {
         getJobs(req, res, storedFilterParams[req.get('host')]);
     } else {
@@ -81,16 +82,19 @@ function jenkinsConnect() {
     jenkins = jenkinsApi.init(format(config.cfg.jenkinsUrl, config.cfg.user, config.cfg.pass));
 }
 
-function getJobs(req, res, filter) {
+function getJobs(req, res, filterOb) {
     jenkins.all_jobs(function(err, data){
         if(err) {
             console.error(err);
             res.redirect('/setup');
             return;
         }
-        if(filter) {
+        if(filterOb) {
             console.log('filtering results...');
-            data = data.filter(function(d){return d.name.indexOf(filter.name) > -1});
+            var parsedFilter = filter.parse(filterOb.name);
+            data = data.filter(function(d) {
+                return filter.matches(d.name, parsedFilter);
+            });
         }
         res.send(data);
     });
---
+++ utils/filter.js
@@ -0,0 +1,49 @@
+var lexer=require('lex')
+var lodash=require('lodash')
+var util=require('util')
+
+var lex = new lexer();
+lex.addRule(/[+]/, function(l) {
+    lex.logicChanged = true;
+    lex.currentLogic='AND';
+});
+lex.addRule(/[-]/, function(l) {
+    lex.logicChanged = true;
+    lex.currentLogic='NOT';
+});
+lex.addRule(/./, function(c) {
+    //sanitize for old use without and/or
+    if(!lex.currentLogic) {
+        lex.logicChanged = true;
+        lex.currentLogic = 'AND';
+    }
+    var lArray = lex.filter[lex.currentLogic];
+    if(!lArray) {
+        lex.filter[lex.currentLogic] = [];
+        lArray = lex.filter[lex.currentLogic];
+    }
+    if(lex.logicChanged) {
+        lArray[lArray.length] = '';
+        lex.logicChanged = false;
+    }
+    lArray[lArray.length - 1] = lArray[lArray.length - 1] + c;
+});
+
+function parseFilter(filterString) {
+    lex.filter = {};
+    lex.currentLogic = undefined;
+    lex.setInput(filterString);
+    lex.lex();
+    return lex.filter;
+}
+
+function matches(str, filter) {
+    var gotThisAND = lodash.filter(filter.AND, function(n){return str.indexOf(n) > -1;});
+    var gotThisNOT = lodash.filter(filter.NOT, function(n){return str.indexOf(n) > -1;});
+    return gotThisAND.length > 0 && gotThisNOT.length === 0;
+}
+
+module.exports = {
+  parse: parseFilter,
+  matches: matches
+}
